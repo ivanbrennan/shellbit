56fdb8882ad66a9dd5c897aeabca58735f224682
ivanbrennan <ivan.brennan@gmail.com>
2019-06-17 00:27:58 -0400

    clean up Git

--- src/NixShellBit/Git.hs
+++ src/NixShellBit/Git.hs
@@ -20,7 +20,7 @@ import Bindings.Libgit2   (C'git_strarray(C'git_strarray),
                            c'git_remote_load, c'git_remote_free,
                            withLibGitDo)
 import Control.Exception  (finally)
-import Control.Monad      (when, (>=>))
+import Control.Monad      (when, (<=<), (>=>))
 import Data.List          (intercalate)
 import Foreign            (Ptr, alloca, peek, sizeOf, allocaBytes,
                            fromBool, plusPtr)
@@ -29,16 +29,22 @@ import Foreign.C.Types    (CChar, CInt, CSize)
 import NixShellBit.PPrint (Doc, red, die, text)
 
 
-gitDiscoverRepo
-  :: FilePath
-  -> [String]
-  -> IO (Maybe FilePath)
+gitDiscoverRepo :: FilePath -> [String] -> IO (Maybe FilePath)
 gitDiscoverRepo startPath ceilingDirs =
     withLibGitDo $
-    withCString startPath  $ \start_path ->
+    withCString startPath  $ \start_path   ->
       withCString ceiling' $ \ceiling_dirs ->
-        allocaBytes bytes  $ \path_out ->
-          discover start_path ceiling_dirs path_out
+        allocaBytes bytes  $ \path_out     ->
+          do
+            exit <- c'git_repository_discover path_out
+                                              path_size
+                                              start_path
+                                              across_fs
+                                              ceiling_dirs
+            case exit of
+              x | x == c'GIT_OK        -> Just <$> peekCString path_out
+                | x == c'GIT_ENOTFOUND -> pure Nothing
+                | otherwise            -> fatal "c'git_repository_discover"
   where
     ceiling' :: String
     ceiling' = intercalate ":" ceilingDirs
@@ -52,36 +58,20 @@ gitDiscoverRepo startPath ceilingDirs =
     across_fs :: CInt
     across_fs = fromBool False
 
-    discover
-      :: CString
-      -> CString
-      -> Ptr CChar
-      -> IO (Maybe FilePath)
-    discover start_path ceiling_dirs path_out = do
-      exit <- c'git_repository_discover
-                path_out path_size start_path across_fs ceiling_dirs
 
-      case exit of
-        e | e == c'GIT_OK        -> Just <$> peekCString path_out
-          | e == c'GIT_ENOTFOUND -> pure Nothing
-          | otherwise            -> fatal "c'git_repository_discover"
-
-
-gitRemoteList
-  :: FilePath
-  -> IO [String]
+gitRemoteList :: FilePath -> IO [String]
 gitRemoteList repoPath =
     withLibGitDo $
     withRepo repoPath $ \repo ->
-      alloca          $ \out -> do
-        exit <- c'git_remote_list out repo
-        checkError exit "c'git_remote_list"
-        C'git_strarray cstrings count <- peek out
-        mapM
-          (\i ->
-            peek (plusPtr cstrings (i * step)) >>= peekCString
-          )
-          [0 .. fromIntegral (count - 1)]
+      alloca          $ \out  ->
+        do
+          exit <- c'git_remote_list out repo
+          checkError exit "c'git_remote_list"
+          C'git_strarray cstrings count <- peek out
+
+          mapM
+            (peekCString <=< (peek . plusPtr cstrings) . (step *))
+            [0 .. fromIntegral (count - 1)]
   where
     step :: Int
     step = sizeOf (undefined :: Ptr CString)
@@ -133,15 +123,17 @@ bracketResource
   -> (Ptr a -> IO b)
   -> IO b
 bracketResource initName initCmd free f =
-    alloca $ \pptr -> do
-      initialize pptr
-      resource <- peek pptr
-      run resource
+    alloca $ \pptr ->
+      do
+        initialize pptr
+        resource <- peek pptr
+        run resource
   where
     initialize :: Ptr (Ptr a) -> IO ()
-    initialize pptr = do
-      exit <- initCmd pptr
-      checkError exit initName
+    initialize pptr =
+      do
+        exit <- initCmd pptr
+        checkError exit initName
 
     run :: Ptr a -> IO b
     run resource =
@@ -163,6 +155,7 @@ fatal cmd =
 
 
 gitErrMsg :: IO String
-gitErrMsg = do
-  err <- peek =<< c'giterr_last
-  peekCString (c'git_error'message err)
+gitErrMsg =
+  do
+    err <- peek =<< c'giterr_last
+    peekCString (c'git_error'message err)
